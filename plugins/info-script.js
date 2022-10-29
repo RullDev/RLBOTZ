af85d92bb583156a8deffb3f57443845a0b5caa6
RullDev <105213064+RullDev@users.noreply.github.com>
2022-10-29 11:22:06 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,7 +5,7 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 
 let handler = async function (m, { text, usedPrefix, command }) {
 
-let info = `❏ *📮 Script SilenceBOT*\n│• *Script :*\n│↳ Private ya hihihiヾ(〃^∇^)ﾉ\n│• *Node_Modules:*\n│↳ https://semawur.com/mwuPPdYT\n│↳ Ato tekan *npm  i/yarn install*\n│• *Base :* \n│↳ github.com/bochilgaming/games-wabot-md/\n┗──────────═┅═──────────\n\n📍 *N o t e :* \n• Script ada di Github:v\n• Jangan Lupa kasih star, follow & kasih credit\n• *Dilarang menjual* Script Ini!\n• Jika menemukan bug di script, harap lapor owner\n• Github SilenceBOT: https://github.com/CyexSynMD Jangan, lupa follow github\n• Bot ini free untuk digunakan di grup ini, kalo mau memasukan BOT ke grup lain minta izin owner dulu ya:)`
+let info = `❏ *📮 Script RullBOT*\n│• *Script :*\n│↳ Private ya hihihiヾ(〃^∇^)ﾉ\n│• *Node_Modules:*\n│↳ https://semawur.com/mwuPPdYT\n│↳ Ato tekan *npm  i/yarn install*\n│• *Base :* \n│↳ github.com/bochilgaming/games-wabot-md/\n┗──────────═┅═──────────\n\n📍 *N o t e :* \n• Script ada di Github:v\n• Jangan Lupa kasih star, follow & kasih credit\n• *Dilarang menjual* Script Ini!\n• Jika menemukan bug di script, harap lapor owner\n• Websites RullBOT: https://bit.ly/3eggcxd Jangan, lupa join grup ya\n• Bot ini free untuk digunakan di grup ini, kalo mau memasukan BOT ke grup lain minta izin owner dulu ya:)`
 let kled = `https://telegra.ph/file/ff053a2dff6c14bcb0578.jpg`
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
@@ -22,7 +22,7 @@ let buttonMessage= {
 'mediaUrl':sig,
 'mediaType':2,
 'previewType':'pdf',
-'title':'𝗦𝗺𝗶𝗹𝗲𝘆𝗕𝗼𝘁𝘇~',
+'title':'𝙍𝙪𝙡𝙡𝘽𝙊𝙏~',
 'body':titlebot,
 'thumbnail':await(await fetch(kled)).buffer(),
 'sourceUrl':'https://www.instagram.com/dvekgw'}},
